3f6417eaeff293f726575672adad4a2c568c7e8f
catrionapharkins <catrionapharkins@gmail.com>
2019-11-07 18:50:12 -0500

    add some documentation

--- 2019-11-07/local_modules/last_week.py
+++ 2019-11-07/local_modules/last_week.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 def generate_test_tree(test_dir,overwrite=False):
+    """
+    
+    Some text placed here
+    
+    """
     seasons = 'spring summer autumn winter'.split()
     animals = 'cat dog bat monkey elephant'.split()
 
